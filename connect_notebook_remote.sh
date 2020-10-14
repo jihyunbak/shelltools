bb3581b8bcb54947f940eb9890c0e2c6ae8fb801
Ji Hyun Bak <jihyun.bak@gmail.com>
2020-10-14 13:35:42 -0700

    add script to connect to remote notebook server

---
+++ connect_notebook_remote.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HOSTNAME=$1
+
+# match default port number
+case $HOSTNAME in
+
+    node1-2)
+        PORT_DEF=9999
+        ;;
+    
+    virga-05)
+        PORT_DEF=8851
+        ;;
+
+    *)
+        echo "warning: unknown host"
+        PORT_DEF=8888
+        ;;
+esac
+
+PORTNUM=${2:-$PORT_DEF}
+
+# to connect to remove notebook server
+# syntax: [local_port]:localhost:[remote_port]
+
+ssh -L ${PORTNUM}:localhost:${PORTNUM} $HOSTNAME
+
+# then open browser and go to
+# `localhost:[local_port]`
+
